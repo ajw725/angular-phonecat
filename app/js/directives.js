d6dab66445c6ae96020e6b5e92a3d5e225df19c9
Andrew Weinstein <ajw725@comcast.net>
2016-04-01 17:17:50 -0400

    Use directive for each entry in leaderboard

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,15 @@
 'use strict';
 
 /* Directives */
+
+var ajwDirectives = angular.module( 'ajwDirectives', [] );
+
+ajwDirectives.directive( 'leaderInfo', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      leader: '='
+    },
+    templateUrl: 'partials/leaderInfo.html'
+  };
+});
