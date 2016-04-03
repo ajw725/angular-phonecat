5531a9777dcb94f407d27dfe2d07d754e47db1cc
Andrew Weinstein <ajw725@comcast.net>
2016-04-03 16:54:27 -0400

    Track change in rank each time leaderboard is refreshed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,17 +8,28 @@ var min = 0;
 var max = 10;
 var step = 10;
 
-var boardData = [];
-
 ajwControllers.controller('LeaderboardCtrl', ['$scope', '$http', '$timeout',
                                           function($scope, $http, $timeout) {
+  
+  var oldRanks = {},
+      newRanks = {},
+      boardData = [];
   
   function updateData() {
     $http.get( 'https://apis.trainheroic.com/public/leaderboard/468425' )
       .success( function(data) {
+        oldRanks = newRanks;
+        newRanks = {};
         boardData = data.results;
+        
+        for( var i = 0; i < boardData.length; i++ ) {
+          var leader = boardData[i];
+          newRanks[leader.username] = leader.rank;
+        }
+        
         $scope.workoutName = data.workoutTitle;
         $scope.date = new Date( data.date );
+        $scope.tests = data.tests;
         
         min = 0;
         max = 10;
@@ -28,8 +39,8 @@ ajwControllers.controller('LeaderboardCtrl', ['$scope', '$http', '$timeout',
         nextGroup();
       })
       .error( function(err) {
-        console.log( 'oops' );
-        $timeout( updateData, 10000 ); // try again in a minute if error
+        console.log( 'An error occurred fetching data' );
+        $timeout( updateData, 10000 ); // try again in 10 seconds if error
       });
   };
   
@@ -37,16 +48,46 @@ ajwControllers.controller('LeaderboardCtrl', ['$scope', '$http', '$timeout',
     if( max >= boardData.length ) {
       max = boardData.length;
       $scope.leaders = boardData.slice( min, max );
+      
+      for( var i = 0; i < $scope.leaders.length; i++ ) {
+        var leader = $scope.leaders[i];
+        $scope.leaders[i].rankChange = rankChange( leader.username );
+      }
+      
       $scope.minIdx = min + 1;
       $scope.maxIdx = max;
       $timeout( updateData, 10000 );
     } else {
       $scope.leaders = boardData.slice( min, max );
+      
+      for( var i = 0; i < $scope.leaders.length; i++ ) {
+        var leader = $scope.leaders[i];
+        var change = rankChange( leader.username );
+        if( change > 0 ) {
+          $scope.leaders[i].rankChange = '+' + change.toString();
+          $scope.leaders[i].rankClass = "rank-change-up";
+        } else if( change < 0 ) {
+          $scope.leaders[i].rankChange = '-' + change.toString();
+          $scope.leaders[i].rankClass = "rank-change-down";
+        } else {
+          $scope.leaders[i].rankChange = '+0';
+          $scope.leaders[i].rankClass = "rank-change";
+        }
+      }
+      
       $scope.minIdx = min + 1;
       $scope.maxIdx = max;
       min = max;
       max += step;
       $timeout( nextGroup, 10000 );
+    }
+  };
+  
+  function rankChange( username ) {
+    if( username in oldRanks && username in newRanks ) {
+      return newRanks[username] - oldRanks[username]
+    } else {
+      return '0';
     }
   };
   
